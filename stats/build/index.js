536702242a87e1613df764d8f535615f37cea30c
magaz1nes <letuss004@gmail.com>
2022-05-09 23:35:37 +0700

    commit

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,12 +1,16 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
-const matches = fs_1.default.readFileSync('../football.csv', { encoding: 'utf-8' })
-    .split('\n')
-    .map((row) => {
-    return [];
-});
-console.log(matches);
+const CsvFileReader_1 = require("./CsvFileReader");
+const MatchResult_1 = require("./MatchResult");
+const reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+let winsOfMU = 0;
+for (let col of reader.data) {
+    if (col[1] === 'Man United' && col[5] === MatchResult_1.MatchResult.HomeWin) {
+        winsOfMU++;
+    }
+    else if (col[2] === 'Man United' && col[5] === MatchResult_1.MatchResult.AwayWin) {
+        winsOfMU++;
+    }
+}
+console.log("MU wins: ", winsOfMU);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,11 +1,15 @@
-import fs from 'fs';
+import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
 
-const matches = fs.readFileSync('../football.csv', {encoding: 'utf-8'})
-  .split('\n')
-  .map((row:string):string[] => {
+const reader = new CsvFileReader('football.csv');
+reader.read();
+let winsOfMU = 0;
+for (let col of reader.data) {
+  if (col[1] === 'Man United' && col[5] === MatchResult.HomeWin) {
+    winsOfMU++;
+  } else if (col[2] === 'Man United' && col[5] === MatchResult.AwayWin) {
+    winsOfMU++;
+  }
+}
 
-    return [];
-  });
-
-
-console.log(matches);
+console.log("MU wins: ", winsOfMU);
